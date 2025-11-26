11e7da23acaef28a76bb54880920c31f062f4cd1
NicKoehler <grillinicola@proton.me>
2025-11-26 14:28:27 +0100

    fix deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,8 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter({ fallback: '404.html' }) },
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+	kit: { adapter: adapter({ fallback: '404.html' }) }
 };
 
 export default config;
